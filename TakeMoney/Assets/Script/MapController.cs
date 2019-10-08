68d8f6da6db6039f22d8eab24e9d4a9f29a435c0
shogo0710 <47047749+shogo0710@users.noreply.github.com>
2019-10-08 13:04:35 +0900

    FirstCommit

---
+++ TakeMoney/Assets/Script/MapController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+    public GameObject undermapPre;
+    public GameObject undermap;
+    int mapcount = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(MapGenerator());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator MapGenerator()
+    {
+        while (true)
+        {
+            Vector3 firstpos = undermap.transform.position;
+            Debug.Log(firstpos);
+            mapcount++;
+            firstpos.x += 23.04f * mapcount;
+            Debug.Log(firstpos);
+            GameObject tuika = Instantiate(undermapPre, firstpos, transform.rotation) as GameObject;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
